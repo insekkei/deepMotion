76d66316287d37e147f079eef6cde33bd7c12c0e
Chi Zhang <chizhang.sysu@gmail.com>
2017-10-10 08:55:38 +0800

    Updated product description and team description in home.js

--- app/assets/config/home.js
+++ app/assets/config/home.js
@@ -97,29 +97,29 @@ const text = {
     ITEMS: [{
       TITLE: {
         EN: 'DeepMotion Client: A sensor that sees miles ahead',
-        ZH: 'DM-100：视觉感知遥遥领先的传感器'
+        ZH: 'DeepMotion客户端：你所拥有的不仅仅是ADAS'
       },
       DESCRIPTION: {
         EN: 'The DeepMotion sensor DM-100 is an intelligent ADAS device made of consumer-level stereo cameras, GPS and IMU modules. It not only provides state-of-the-arts ADAS functions such as Lane Departure Warning, Pedestrian Detection, Collision Warning, but also performs localization at 10cm accuracy without replying on RTK. Equipped with our pre-installed HD maps, DM-100 is able to perform environment sensing tasks with less computing power, localize itself in GPS-denied environment, collect trajectories of road agents, and report map changes in real-time.',
-        ZH: 'DM-100是由消费级立体相机，GPS和IMU模块组成的只能ADAS设备。它不仅提供最先进的ADAS功能，如车道偏离警告，行人侦测，碰撞预警，还可以以10厘米的精度执行定位，而无需恢复RTK。预存了本地的高清地图，DM-100可以以较少的计算对环境进行感测，在无GPS的环境中定位，收集道路的信息，并实时报告地图变化。'
+        ZH: 'DeepMotion车载传感器DM-100通过精心设计的算法将一套低成本的多目相机和GPS惯导模组融合在一起，不仅提供了高可靠的 车规级的ADAS功能，例如车道线保持，行人检测，碰撞报警等等，还可提供高精度的车辆定位能力。尤其是当车辆预装了我们的高精度地图后，DM-100可以在不依赖任何RTK等高成本技术的条件下，实现全场景10cm精度定位。此功能对于GPS信号遮挡的区域尤其重要，为Level4及以上驾驶场景提供必备保障。在提供ADAS和定位服务的同时，本设备还为新地图生成以及地图更新提供支持，是我们众包地图方案中的重要一环。'
       },
       LABELS: {
         EN: 'Consumer-level | Most advanced ADAS | Real-time',
-        ZH: '消费级立体相机 ｜ 最先进的ADAS ｜ 10厘米精度 ｜ 实时'
+        ZH: '低成本 | 车规级ADAS功能 | 全场景10厘米定位| 地图制作与更新'
       },
       IMAGE: '/config/resource/products/product1.png'
     }, {
       TITLE: {
         EN: 'DeepMotion Cloud: An AI that fuses billion miles of data.',
-        ZH: 'DeepMotion Cloud：融合数十亿里道路数据的人工智能'
+        ZH: 'DeepMotion云端：一个融合亿万公里数据的地图整合引擎'
       },
       DESCRIPTION: {
         EN: 'The DeepMotion cloud receives and fuses billions of observations from our crowdsourced DM-100s into a consistent 3D semantic maps. Our AI replaces the traditional labor-intensive maps production pipeline by itself, which produces the maps at much higher accuracy, much faster speed and much lower cost.',
-        ZH: 'DeepMotion Cloud接收并融合来自我们众包DM-100的数十亿观测值，成一个一致的3D语义地图。我们的人工智能取代了传统的劳动密集型地图人工标注流水线，它以更高的精度、更快的速度和更低的成本生成地图。'
+        ZH: 'DeepMotion云端引擎通过处理由装配DM-100设备的车辆采集的海量道路数据，优化出一套实时、高一致性、高精度的地图。每台DM-100设备上传到云端的数据控制在每公里平均30K。我们通过用AI算法取代传统地图制作中的大量人工劳动，实现了一套高度自动化的地图生产系统。从而在加快地图生产速度，降低生产成本的同时，获得了更高精度的地图。目前，我们系统的自动化程度接近100%。'
       },
       LABELS: {
         EN: '3D semantic maps | 10cm Accuracy | Crowdsourced',
-        ZH: '十亿级别观测值 ｜ 3D语义地图 ｜ 众包'
+        ZH: '3D语义地图 | 众包 | 高度自动化'
       },
       IMAGE: '/config/resource/products/product2.png'
     }]
@@ -160,7 +160,7 @@ const text = {
       },
       TITLE: {
         EN: 'Co-founder & CTO',
-        ZH: '联合创始人 ／ CEO'
+        ZH: '联合创始人 / CTO'
       },
       QUALIFICATIONS: {
         YEARS: 10,
@@ -168,8 +168,9 @@ const text = {
         PAPER: 50
       },
       INTRODUCTION: {
-        EN: 'Lead researcher from Microsoft Research. Developed 6DoF tracking and 3D surface reconstruction algorithms for Microsoft Hololens.',
-        ZH: '来自微软研究院的首席研究员，在SLAM、立体视觉和深度学习领域深耕多年，为Hololens提供3D表面重建算法。'
+
+        EN: 'Lead researcher from Microsoft Research. Published 50+ papers, holding 20+ US/International patents. Developed 6DoF tracking and 3D surface reconstruction algorithms for Microsoft Hololens.  ',
+        ZH: '×微软研究院主管研究员 ×发表50+国际论文，拥有20+国际专利 ×SLAM、三维视觉专家，为Hololens 6-DoF跟踪及表面重建提供核心算法。'
       },
       PAPERLINK: 'https://scholar.google.com.sg/citations?user=iL8SeoEAAAAJ&hl=en&oi=ao'
     }, {
@@ -179,7 +180,7 @@ const text = {
       },
       TITLE: {
         EN: 'Co-founder & CEO',
-        ZH: '联合创始人 ／ CEO'
+        ZH: '联合创始人 / CEO'
       },
       QUALIFICATIONS: {
         YEARS: 10,
@@ -187,8 +188,8 @@ const text = {
         PAPER: 50
       },
       INTRODUCTION: {
-        EN: 'Lead researcher from Microsoft Research. Developed core algorithms for Microsoft Hololens, Bing, and 3D Builder.',
-        ZH: '来自微软研究院的首席研究员，曾为Hololens、Bing图片搜索，3D打印等微软重量级产品提供核心算法。'
+        EN: 'Lead researcher from Microsoft Research. Published 50+ papers, holding 20+ US/International patents. Developed core algorithms for Microsoft Hololens, Bing, and 3D Builder.  ',
+        ZH: '×微软研究院主管研究员 ×发表50+国际论文，拥有20+国际专利 ×SLAM、三维视觉专家，为Hololens、Bing图片搜索，3D打印等微软重量级产品提供核心算法。'
       },
       PAPERLINK: 'https://scholar.google.com.sg/citations?user=6WCyi64AAAAJ&hl=en'
     }, {
@@ -198,7 +199,7 @@ const text = {
       },
       TITLE: {
         EN: 'Co-founder & Chief Scientist',
-        ZH: '联合创始人 ／ 首席科学家'
+        ZH: '联合创始人 / 首席科学家'
       },
       QUALIFICATIONS: {
         YEARS: 10,
@@ -206,18 +207,18 @@ const text = {
         PAPER: 30
       },
       INTRODUCTION: {
-        EN: 'Researcher from Microsoft Research. Expert on deep learning and computer vision. BS and PhD from USTC.',
-        ZH: '来自微软研究院的研究员，深度学习和计算机视觉方向的专家。'
+        EN: 'Researcher from Microsoft Research.  Published 30+ papers, holding 5 US/International patents.  Expert on deep learning and computer vision. Provides core algorithms for Microsoft Xiaoice and Microsoft Conigtive Service.',
+	   	ZH: '×微软研究院研究员 ×发表30+国际论文，拥有5项国际专利 ×深度学习和计算机视觉专家，为微软小冰、微软视觉认知服务提供核心算法。'
       },
       PAPERLINK: 'https://sites.google.com/site/kuiyuanyang/'
     }, {
       NAME: {
         EN: 'Chi Zhang',
-        ZH: '张驰'
+        ZH: '张弛'
       },
       TITLE: {
         EN: 'Co-founder & R&D Director',
-        ZH: '联合创始人 ／ CEO'
+        ZH: '联合创始人 / 研发总监'
       },
       QUALIFICATIONS: {
         YEARS: 10,
@@ -225,9 +226,8 @@ const text = {
         PAPER: 50
       },
       INTRODUCTION: {
-        EN: 'Published papers on CVPR/ ICCV/ ECCV/ TVCG. Expert on stereo vision. Ranked 1st at Middlebury stereo 3.0 two times.',
-        ZH: '来自微软研究院的首席研究员，Hololens的3D表面重建算法，BS from PKU， PhD from SJTU.'
-      },
+        EN: 'Joint PhD from Sun Yat-Sen University and Microsoft Research.  Published papers on CVPR/ICCV/ECCV/TVCG.  Expert on stereo vision. Ranked 1st at Middlebury stereo 3.0 two times. ',
+        ZH: '×中山大学微软研究院联合培养博士 ×在顶级国际会议期刊CVPR/ICCV/ECCV/TVCG上发表论文 ×双目立体视觉专家，两次在Middlebury Stereo排名第一。'},
       PAPERLINK: 'http://chizhang.me/'
     }]
   },
